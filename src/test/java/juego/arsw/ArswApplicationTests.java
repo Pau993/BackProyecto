dcea549c9daef32c37d727aa7e9133cd7f3e32da
ManuelB16 <felipebarrera104@gmail.com>
2025-05-20 18:45:01 -0500

    Pruebas 80

--- src/test/java/juego/arsw/ArswApplicationTests.java
+++ src/test/java/juego/arsw/ArswApplicationTests.java
@@ -2,12 +2,27 @@ package juego.arsw;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class ArswApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private ApplicationContext applicationContext;
 
+    @Test
+    void contextLoads() {
+        // Verifica que el contexto de Spring se carga correctamente
+        assertNotNull(applicationContext);
+    }
+
+    @Test
+    void mainMethodShouldStartWithoutExceptions() {
+        // Simular llamada al método main
+        // Este test básicamente verifica que no se lanzan excepciones
+        ArswApplication.main(new String[]{});
+    }
 }
